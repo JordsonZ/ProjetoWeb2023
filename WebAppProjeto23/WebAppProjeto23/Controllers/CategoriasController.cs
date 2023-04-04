d34f5e7331a8008d419bd4751cb925d32238fbe8
Jordson Albino Galvão Tavares <jordson.galvao@academico.ifrn.edu.br>
2023-04-04 12:31:57 -0300

    Inclusão do Projeto

---
+++ WebAppProjeto23/WebAppProjeto23/Controllers/CategoriasController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjeto23.Models;
+
+namespace WebAppProjeto23.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria() { CategoriaId = 1, Nome = "Notebooks"},
+            new Categoria() { CategoriaId = 2, Nome = "Monitores"},
+            new Categoria() { CategoriaId = 3, Nome = "Impressoras"},
+            new Categoria() { CategoriaId = 4, Nome = "Mouses"},
+            new Categoria() { CategoriaId = 5, Nome = "Desktops"}
+        };
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(categorias);
+        }
+    }
+}
---
+++ WebAppProjeto23/WebAppProjeto23/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto23.Models
+{
+    public class Categoria
+    {
+        public long CategoriaId { get; set; }
+        public string Nome { get; set; }
+    }
+}
