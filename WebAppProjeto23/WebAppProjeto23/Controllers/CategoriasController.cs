7469917722660a99f13a805f2ba6e2170815a994
Jordson Albino Galvão Tavares <jordson.galvao@academico.ifrn.edu.br>
2023-04-11 15:47:02 -0300

    Alterações no Projeto

--- WebAppProjeto23/WebAppProjeto23/Controllers/CategoriasController.cs
+++ WebAppProjeto23/WebAppProjeto23/Controllers/CategoriasController.cs
@@ -22,5 +22,47 @@ namespace WebAppProjeto23.Controllers
         {
             return View(categorias);
         }
+        // GET: Categorias
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            categorias.Add(categoria);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaId == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            categorias.Remove(
+            categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            categorias.Add(categoria);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaId == id).First());
+        }
+        public ActionResult Delete(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaId == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Categoria categoria)
+        {
+            categorias.Remove(
+            categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            return RedirectToAction("Index");
+        }
     }
 }
