e1a734d98d063557f28f2a4de0af368b0a5932a7
Jordson Albino Galvão Tavares <jordson.galvao@academico.ifrn.edu.br>
2023-04-17 14:38:21 -0300

    Alteração

---
+++ WebAppProjeto23/WebAppProjeto23/Context/EFContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto23.Models
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
---
+++ WebAppProjeto23/WebAppProjeto23/Models/Fabricante.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto23.Models
+{
+    public class Fabricante
+    {
+        public long FabricanteId { get; set; }
+        public string Nome { get; set; }
+    }
+}
