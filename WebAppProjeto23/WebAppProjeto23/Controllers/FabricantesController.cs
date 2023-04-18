55bea54e7588fb6f7a7d4f106806bcb425c64e4d
Jordson Albino Galvão Tavares <jordson.galvao@academico.ifrn.edu.br>
2023-04-18 15:52:12 -0300

    ALTERAÇÃO

---
+++ WebAppProjeto23/WebAppProjeto23/Controllers/FabricantesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjeto23.Models;
+
+namespace WebAppProjeto23.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(
+                context.Fabricantes.OrderBy(c => c.Nome)
+            );
+        }
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
