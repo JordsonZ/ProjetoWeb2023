26f2b9893fe9c8c888caddb8abee3d736a0ea568
Jordson Albino Galvão Tavares <jordson.galvao@academico.ifrn.edu.br>
2023-05-08 14:24:57 -0300

    conclusão cap3

--- WebAppProjeto23/WebAppProjeto23/Controllers/FabricantesController.cs
+++ WebAppProjeto23/WebAppProjeto23/Controllers/FabricantesController.cs
@@ -96,6 +96,7 @@ namespace WebAppProjeto23.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante " + fabricante.Nome.ToUpper() + " foi removido";
             return RedirectToAction("Index");
         }
     }
