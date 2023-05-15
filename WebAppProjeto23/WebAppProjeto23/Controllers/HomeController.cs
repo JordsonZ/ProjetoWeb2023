7ead7417d28afbe82fb93fe3de5890786e6a176e
Jordson Albino Galvão Tavares <jordson.galvao@academico.ifrn.edu.br>
2023-05-15 14:20:46 -0300

    Atividade2

---
+++ WebAppProjeto23/WebAppProjeto23/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjeto23.Models;
+
+namespace WebAppProjeto23.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            Home h = new Home();
+            h.fabricantes = context.Fabricantes.OrderBy(c => c.Nome);
+            h.categorias = context.Categorias.OrderBy(c => c.Nome);
+
+            return View(h);
+        }
+    }
+}
---
+++ WebAppProjeto23/WebAppProjeto23/Models/Home.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto23.Models
+{
+    public class Home
+    {
+        public IQueryable<Fabricante> fabricantes;
+        public IQueryable<Categoria> categorias;
+    }
+}
