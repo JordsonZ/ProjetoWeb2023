6d98d198aba125681016a026f320ee47546c3fec
20211011110020 <20211011110020@CNAT391785>
2023-09-25 14:27:35 -0300

    autenticacao

---
+++ WebAppProjeto23/WebAppProjeto23/Areas/Seguranca/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjeto23.Infraestrutura;
+
+namespace WebAppProjeto23.Areas.Seguranca.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Seguranca/Admin
+            // Definição da Propriedade GerenciadorUsuario
+            private GerenciadorUsuario GerenciadorUsuario
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().
+                GetUserManager<GerenciadorUsuario>();
+
+            }
+        }
+        // GET: Seguranca/Admin
+            public ActionResult Index()
+            {
+            return View(GerenciadorUsuario.Users);
+            }
+    }
+}
