23b63743cf119db2abdb5eba121fb117d3ced8b7
20211011110020 <20211011110020@CNAT391785>
2023-09-25 14:23:26 -0300

    segurança add

---
+++ WebAppProjeto23/WebAppProjeto23/App_Start/IdentityConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppProjeto23.DAL;
+using WebAppProjeto23.Infraestrutura;
+
+namespace WebAppProjeto23.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IdentityDbContextAplicacao>(IdentityDbContextAplicacao.Create);
+            app.CreatePerOwinContext<GerenciadorUsuario>(GerenciadorUsuario.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Seguranca/Account/Login"),
+            });
+        }
+    }
+}
+ 
---
+++ WebAppProjeto23/WebAppProjeto23/Areas/Seguranca/Data/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto23.Areas.Seguranca.Data
+{
+    public class Usuario : IdentityUser
+    {
+
+    }
+}
